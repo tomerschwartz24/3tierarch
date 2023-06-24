ea586acf69c0df5184ba3017dd3a5be824d81768
tomerschwartz2411 <tomerschwartz2411@gmail.com>
2023-06-24 15:46:15 +0300

    added map_public_ip_on_launch for ec2 instance inside the public subnets, which means they will have a public IP assigned to them

--- VPC-Groundlayer.tf
+++ VPC-Groundlayer.tf
@@ -14,6 +14,7 @@ resource "aws_subnet" "public_subnets" {
  vpc_id     = aws_vpc.main.id
  cidr_block = element(var.public_subnets, count.index)
  availability_zone = element(var.azs, count.index)
+ map_public_ip_on_launch = true
  
  tags = {
    Name = "3tier-arch-Public Subnet ${count.index + 1}"
