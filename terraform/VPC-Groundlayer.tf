a7782c18d186a7679b1a0dd46d1fa8fae2a608db
tomerschwartz2411 <tomerschwartz2411@gmail.com>
2023-07-10 16:05:59 +0300

    change structure

--- terraform/VPC-Groundlayer.tf
+++ terraform/VPC-Groundlayer.tf
@@ -1,14 +1,14 @@
-#Create a VPC resource named 3tier-arch 
+#Create a VPC resource 
 resource "aws_vpc" "main" {
- cidr_block = "172.20.0.0/16"
+ cidr_block = "${var.vpc_cidr_block}"
  enable_dns_hostnames = true
 
  tags = {
-   Name = "3tier-arch"
+   Name = "${var.vpc_name}"
  }
 }
 
-#Create public subnets (2 in total for this 3 tier architecture mock project)
+
 resource "aws_subnet" "public_subnets" {
  count      = length(var.public_subnets)
  vpc_id     = aws_vpc.main.id
@@ -17,11 +17,10 @@ resource "aws_subnet" "public_subnets" {
  map_public_ip_on_launch = true
  
  tags = {
-   Name = "3tier-arch-Public Subnet ${count.index + 1}"
+   Name = "${var.public_subnet_name}-${count.index + 1}"
  }
 }
  
-#Create private subnets (6 In total for this mock 3tier architecture mock project)
 resource "aws_subnet" "private_subnets" {
  count      = length(var.private_subnets)
  vpc_id     = aws_vpc.main.id
@@ -29,80 +28,80 @@ resource "aws_subnet" "private_subnets" {
  availability_zone = element(var.azs, count.index)
  
  tags = {
-   Name = "3tier-arch-Private Subnet ${count.index + 1}"
+   Name = "${var.private_subnet_name}-${count.index + 1}"
  }
 }
 
 # Create an Internet Gateway in order to access the internet 
-resource "aws_internet_gateway" "ttier-gw" {
+resource "aws_internet_gateway" "gw" {
  vpc_id = aws_vpc.main.id
  
  tags = {
-   Name = "3tier-arch-IGW"
+   Name = "${var.igw_name}"
  }
 }
 
 
 #Create NAT Gateway at element 0 (First public subnet)
 resource "aws_nat_gateway" "ngw" {
-  allocation_id = "${aws_eip.EIP_3tier_arch.id}"
+  allocation_id = "${aws_eip.elastic_ip.id}"
   subnet_id     = "${aws_subnet.public_subnets[0].id}"
 
   tags = {
-    Name = "3tier-arch-NGW"
+    Name = "${var.ngw_name}"
   }
 }
 
 #Create an Elastic IP for the NAT Gateway
-resource "aws_eip" "EIP_3tier_arch" {
+resource "aws_eip" "elastic_ip" {
   domain = "vpc"
 
   tags = {
-    Name = "3tier-arch-NATGatewayEIP"
+    Name = "${var.EIP_name}"
   }
 }
 
 #Route Table Creation for Public Subnets
-resource "aws_route_table" "three_tier_pub_RT" {
+resource "aws_route_table" "pub_route" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.ttier-gw.id
+    cidr_block = "${var.rt_full_out}"
+    gateway_id = aws_internet_gateway.gw.id
   }
 
   tags = {
-    Name = "three-tier-public-subnet-route"
+    Name = "${var.public_route_name}"
   }
 }
 
 #Route table Association with Public Subnets 
-resource "aws_route_table_association" "three_tier_pub_RT_assoc" {
+resource "aws_route_table_association" "pub_rt_assoc" {
     count          = length(aws_subnet.public_subnets)
     subnet_id      = "${aws_subnet.public_subnets[count.index].id}"
-    route_table_id = "${aws_route_table.three_tier_pub_RT.id}"
+    route_table_id = "${aws_route_table.pub_route.id}"
 }
 
 
 
 
 #Route Table Creation for Private Subnets
-resource "aws_route_table" "three_tier_priv_RT" {
+resource "aws_route_table" "priv_route" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = "${var.rt_full_out}"
     gateway_id = aws_nat_gateway.ngw.id
   }
 
   tags = {
-    Name = "three-tier-private-subnet-route"
+    Name = "${var.private_route_name}"
   }
 }
 
 #Route table Association with Private Subnets 
-resource "aws_route_table_association" "three_tier_priv_RT_assoc" {
+resource "aws_route_table_association" "priv_rt_assoc" {
     count          = length(aws_subnet.private_subnets)
     subnet_id      = "${aws_subnet.private_subnets[count.index].id}"
-    route_table_id = "${aws_route_table.three_tier_priv_RT.id}"
+    route_table_id = "${aws_route_table.priv_route.id}"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,15 +8,64 @@ variable "azs" {
  description = "List of Possible availability zones to be used"
 }
 
-#Public cidrs for 3tier-arch
 
 variable "public_subnets" {
  type        = list(string)
  description = "Public Subnets"
 }
 
-#private cidrs for 3tier-arch
 variable "private_subnets" {
  type        = list(string)
  description = "Private Subnets"
+}
+
+
+variable "public_subnet_name" {
+ type        = string
+ description = "The name of the public subnet"
+}
+
+variable "private_subnet_name" {
+ type        = string
+ description = "The name of the private subnet"
+}
+
+variable "vpc_name" {
+ type        = string
+ description = "The name of the vpc"
+}
+
+variable "vpc_cidr_block" {
+ type        = string
+ description = "The cidr block of the vpc"
+}
+
+variable "igw_name" {
+ type        = string
+ description = "The name of the internet gateway"
+}
+
+variable "ngw_name" {
+ type        = string
+ description = "The name of the nat gateway"
+}
+
+variable "EIP_name" {
+ type        = string
+ description = "The name of the elastic-ip resource gateway"
+}
+
+variable "rt_full_out" {
+ type        = string
+ description = "full access to internet route "
+}
+
+variable "public_route_name" {
+ type        = string
+ description = "Public subnet route name"
+}
+
+variable "private_route_name" {
+ type        = string
+ description = "private subnet route name"
 }
